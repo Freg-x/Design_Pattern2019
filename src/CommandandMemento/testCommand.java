4ecabfdea34d981e9f38513f0b50f31b492d345e
siteng ma <943941437@qq.com>
2019-10-31 00:23:20 +0800

    Command and memento by tanqi

---
+++ src/CommandandMemento/testCommand.java
@@ -0,0 +1,18 @@
+package CommandandMemento;
+
+import CommandandMemento.command.*;
+
+public class testCommand {
+    public static void main(String[] args) {
+        CheckClubLevelRooms room1=new CheckClubLevelRooms();
+        CheckDeluxeRooms room2=new CheckDeluxeRooms();
+        CheckJuniorSuites room3=new CheckJuniorSuites();
+        CheckSignatureSuites room4=new CheckSignatureSuites();
+        Invoker test=new Invoker();
+        test.addCheck(room4);
+        test.addCheck(room3);
+        test.addCheck(room2);
+        test.addCheck(room1);
+        test.launchCheck();
+    }
+}
